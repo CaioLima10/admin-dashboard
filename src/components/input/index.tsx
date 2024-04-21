2a7c7e7f63563ce26cc58e00eee62b07012b16b2
Caio lima <caiohenrys50@gmail.com>
2024-04-20 21:23:48 -0300

    refactor: input add forwardRef

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,13 +1,26 @@
-import { InputHTMLAttributes } from "react";
+import { forwardRef } from "react";
 
-type InputProps = InputHTMLAttributes<HTMLInputElement>;
-
-export function Input(props: InputProps) {
-  return (
-    <input
-      {...props}
-      className="outline-none p-3 w-full max-w-md rounded-md text-base bg-zinc-950/60 
-      border-none ring-1 ring-zinc-800 placeholder:text-zinc-600 text-zinc-600"
-    />
-  );
+interface InputProps {
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+  type?: "password" | "text";
 }
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ value, onChange, placeholder, type = "text" }, ref) => {
+    return (
+      <input
+        ref={ref}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        type={type}
+        className="outline-none p-3 w-full max-w-md rounded-md text-base bg-zinc-950/60 
+        border-none ring-1 ring-zinc-800 placeholder:text-zinc-600 text-zinc-600
+        focus:ring-green-500
+        "
+      />
+    );
+  }
+);
