0202f52126182cb3f354492ef04c0df20000b60c
Caio lima <caiohenrys50@gmail.com>
2024-04-21 04:21:47 -0300

    add style button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -6,7 +6,7 @@ export function Button(props: ButtonProps) {
   return (
     <button
       className="p-2.5 mt-5 outline-none border-none rounded-md w-full cursor-pointer 
-      bg-green-500 text-zinc-100 font-semibold text-base max-w-md"
+      bg-yellow-400 text-zinc-950 font-semibold text-base max-w-md"
       {...props}
     >
       {props.children}
