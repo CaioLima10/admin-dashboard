6a05bb42938a88a77f85b2463608d593559f8e90
Caio lima <caiohenrys50@gmail.com>
2024-04-21 04:25:21 -0300

    image astronaut

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -8,13 +8,18 @@ import { Label } from "../../components/label";
 import { RiEyeLine } from "react-icons/ri";
 import { RiEyeCloseLine } from "react-icons/ri";
 
+import ImageAtronautStudying from "../../assets/astronauta_estudando.png";
+import ImageAtronautFloating from "../../assets/astronauta_flutuando_pelo_espaço.png";
+import { ImageBottomSoon } from "../../components/imageBottomSoon";
+import { emailValidation, nameRegex, passwordRegex } from "./utils/regEx";
+
 export function Signup() {
   const inputRef = useRef<HTMLInputElement>(null);
 
   const navigate = useNavigate();
   const { signup } = useAuth();
 
-  const [user, setUser] = useState("");
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [emailConfirm, setEmailConfirm] = useState("");
   const [password, setPassword] = useState("");
@@ -36,7 +41,7 @@ export function Signup() {
       return;
     }
 
-    const errorMessage = signup(email, password, user);
+    const errorMessage = signup(email, password, name);
 
     if (errorMessage !== null && errorMessage !== undefined) {
       setError(errorMessage);
@@ -47,25 +52,75 @@ export function Signup() {
     navigate("/");
   }
 
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newEmail = event.target.value;
+    setEmail(newEmail);
+    setError("");
+
+    if (!emailValidation(newEmail)) {
+      setError("Endereço de e-mail inválido");
+    } else {
+      setError("");
+    }
+  };
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newName = event.target.value;
+    setName(newName);
+    setError("");
+
+    if (!nameRegex(newName)) {
+      setError("Nome inválido");
+    } else {
+      setError("");
+    }
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newPassword = event.target.value;
+    setPassword(newPassword);
+    setError("");
+
+    if (!passwordRegex(newPassword)) {
+      setError("A senha deve ter pelo menos 6 caracteres");
+    } else {
+      setError("");
+    }
+  };
+
   return (
-    <section className="flex w-full min-h-screen items-center justify-center bg-zinc-950">
-      <div className="hidden md:flex items-center bg-zinc-950 w-full min-h-screen"></div>
+    <section className="flex w-full screen items-center justify-center bg-zinc-950">
+      <div className="hidden md:flex items-center bg-zinc-950 w-full min-h-screen relative">
+        <div className="flex items-center justify-center mx-auto">
+          <img
+            src={ImageAtronautStudying}
+            alt="ASTRONAUTA-ESTUDANDO"
+            className="absolute left-0 bottom-0 z-10"
+          />
+          <ImageBottomSoon />
+          <img
+            src={ImageAtronautFloating}
+            alt="ASTRONAUTA-FLUTUANDO"
+            className="absolute -right-20 -top-2"
+          />
+        </div>
+      </div>
       <form className="w-full max-w-xl min-h-screen bg-zinc-900 flex flex-col items-center justify-center ">
-        <h1 className="mb-4 text-2xl font-bold text-zinc-300">
+        <h1 className="mb-2 text-2xl font-bold text-zinc-300">
           Registre sua conta
         </h1>
         <div className="w-[80%] flex flex-col items-center">
           <Label>Nome</Label>
           <Input
-            value={user}
+            value={name}
             placeholder="Digite seu Nome"
             type="text"
-            onChange={(event) => [setUser(event.target.value), setError("")]}
+            onChange={handleNameChange}
           />
           <Label>E-mail</Label>
           <Input
             value={email}
-            onChange={(event) => [setEmail(event.target.value), setError("")]}
+            onChange={handleEmailChange}
             placeholder="Seu e-mail"
             type="text"
           />
@@ -83,16 +138,13 @@ export function Signup() {
           <div className="w-full max-w-md flex items-center justify-center gap-2">
             <Input
               value={password}
-              onChange={(event) => [
-                setPassword(event.target.value),
-                setError(""),
-              ]}
+              onChange={handlePasswordChange}
               placeholder="Sua senha"
               type={isShowPassword ? "password" : "text"}
               ref={inputRef}
             />
             <button
-              className=" p-2 rounded-full bg-green-500 z-10 text-zinc-900 ring-1 ring-zinc-800"
+              className=" p-2 rounded-full bg-yellow-400 z-10 text-zinc-900 ring-1 ring-zinc-800"
               onClick={() => setIsShowPassword(!isShowPassword)}
               autoFocus
               type="button"
@@ -108,7 +160,7 @@ export function Signup() {
 
           <div className="w-full max-w-md mt-2 ">
             <span className="text-zinc-400">Já tenho conta?</span>
-            <Link className="text-blue-500" to={"/signin"}>
+            <Link className="text-cyan-500 font-bold" to={"/signin"}>
               &nbsp;Entrar
             </Link>
           </div>
