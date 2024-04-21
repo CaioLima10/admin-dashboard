f80e2d763b87ca0f5b3a76d1475b12952ec94cc2
Caio lima <caiohenrys50@gmail.com>
2024-04-20 21:26:21 -0300

    hide and show password entry

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,17 +1,29 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Button } from "../../components/button";
 import { Input } from "../../components/input";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/hooks";
 import { Label } from "../../components/label";
 
+import { RiEyeLine } from "react-icons/ri";
+import { RiEyeCloseLine } from "react-icons/ri";
+
 export function Signin() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const { signin } = useAuth();
   const navigate = useNavigate();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState<string | null>(null);
+  const [isShowPassword, setIsShowPassword] = useState(false);
+
+  useEffect(() => {
+    if (inputRef.current && isShowPassword) {
+      inputRef.current.focus();
+    }
+  }, [isShowPassword]);
 
   function handleLogin() {
     if (!email || !password) {
@@ -30,9 +42,9 @@ export function Signin() {
   }
 
   return (
-    <section className="flex">
-      <div className="hidden md:flex items-center bg-zinc-950 w-full max-w-5xl h-screen"></div>
-      <form className="w-full md:max-w-2xl h-screen bg-zinc-900 flex flex-col items-center justify-center duration-300">
+    <section className="flex w-full min-h-screen items-center justify-center bg-zinc-950">
+      <div className="hidden md:flex items-center bg-zinc-950 w-full min-h-screen"></div>
+      <form className="w-full max-w-xl min-h-screen bg-zinc-900 flex flex-col items-center justify-center">
         <h1 className="mb-4 text-2xl font-bold text-zinc-300">
           Acessar sua conta
         </h1>
@@ -45,15 +57,27 @@ export function Signin() {
             type="text"
           />
           <Label>Senha</Label>
-          <Input
-            value={password}
-            onChange={(event) => [
-              setPassword(event.target.value),
-              setError(""),
-            ]}
-            placeholder="Sua senha"
-            type="password"
-          />
+          <div className="w-full max-w-md flex items-center justify-center gap-2">
+            <Input
+              value={password}
+              onChange={(event) => [
+                setPassword(event.target.value),
+                setError(""),
+              ]}
+              placeholder="Sua senha"
+              type={isShowPassword ? "password" : "text"}
+              ref={inputRef}
+            />
+            <button
+              className=" p-2 rounded-full bg-green-500 z-10 text-zinc-900 ring-1 ring-zinc-800"
+              onClick={() => setIsShowPassword(!isShowPassword)}
+              autoFocus
+              type="button"
+            >
+              {!isShowPassword && <RiEyeLine size={16} />}
+              {isShowPassword && <RiEyeCloseLine size={16} />}
+            </button>
+          </div>
           <span className="flex items-center text-red-500 mt-2">{error}</span>
           <Button type="button" onClick={handleLogin}>
             Registrar
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,18 +1,31 @@
+import { useEffect, useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Input } from "../../components/input";
 import { Button } from "../../components/button";
-import { useState } from "react";
 import { useAuth } from "../../context/hooks";
 import { Label } from "../../components/label";
 
+import { RiEyeLine } from "react-icons/ri";
+import { RiEyeCloseLine } from "react-icons/ri";
+
 export function Signup() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const navigate = useNavigate();
   const { signup } = useAuth();
 
+  const [user, setUser] = useState("");
   const [email, setEmail] = useState("");
   const [emailConfirm, setEmailConfirm] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState<string | null>(null);
+  const [isShowPassword, setIsShowPassword] = useState(false);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isShowPassword]);
 
   function handleSignup() {
     if (!email || !emailConfirm || !password) {
@@ -23,7 +36,7 @@ export function Signup() {
       return;
     }
 
-    const errorMessage = signup(email, password);
+    const errorMessage = signup(email, password, user);
 
     if (errorMessage !== null && errorMessage !== undefined) {
       setError(errorMessage);
@@ -35,13 +48,20 @@ export function Signup() {
   }
 
   return (
-    <section className="flex">
-      <div className="hidden md:flex items-center bg-zinc-950 w-full max-w-5xl h-screen"></div>
-      <form className="w-full md:max-w-2xl h-screen bg-zinc-900 flex flex-col items-center justify-center duration-300">
+    <section className="flex w-full min-h-screen items-center justify-center bg-zinc-950">
+      <div className="hidden md:flex items-center bg-zinc-950 w-full min-h-screen"></div>
+      <form className="w-full max-w-xl min-h-screen bg-zinc-900 flex flex-col items-center justify-center ">
         <h1 className="mb-4 text-2xl font-bold text-zinc-300">
           Registre sua conta
         </h1>
         <div className="w-[80%] flex flex-col items-center">
+          <Label>Nome</Label>
+          <Input
+            value={user}
+            placeholder="Digite seu Nome"
+            type="text"
+            onChange={(event) => [setUser(event.target.value), setError("")]}
+          />
           <Label>E-mail</Label>
           <Input
             value={email}
@@ -60,15 +80,27 @@ export function Signup() {
             type="text"
           />
           <Label>Senha</Label>
-          <Input
-            value={password}
-            onChange={(event) => [
-              setPassword(event.target.value),
-              setError(""),
-            ]}
-            placeholder="Sua senha"
-            type="password"
-          />
+          <div className="w-full max-w-md flex items-center justify-center gap-2">
+            <Input
+              value={password}
+              onChange={(event) => [
+                setPassword(event.target.value),
+                setError(""),
+              ]}
+              placeholder="Sua senha"
+              type={isShowPassword ? "password" : "text"}
+              ref={inputRef}
+            />
+            <button
+              className=" p-2 rounded-full bg-green-500 z-10 text-zinc-900 ring-1 ring-zinc-800"
+              onClick={() => setIsShowPassword(!isShowPassword)}
+              autoFocus
+              type="button"
+            >
+              {!isShowPassword && <RiEyeLine size={16} />}
+              {isShowPassword && <RiEyeCloseLine size={16} />}
+            </button>
+          </div>
           <span className="flex items-center text-red-500 mt-2">{error}</span>
           <Button type="button" onClick={handleSignup}>
             Entrar
