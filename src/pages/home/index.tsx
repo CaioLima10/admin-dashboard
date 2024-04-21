27c5e934fb4f8abd5e6f86a9f1356feecfa63a11
Caio lima <caiohenrys50@gmail.com>
2024-04-20 21:25:39 -0300

    getting first letter of user name

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,10 +4,21 @@ import { useAuth } from "../../context/hooks";
 
 export function HomePage() {
   const navigate = useNavigate();
-  const { signout } = useAuth();
+  const { signout, user } = useAuth();
+
+  const firstLetter = user?.name.substring(0, 1);
 
   return (
     <div>
+      <div className="flex items-center text-base gap-2">
+        <div className="w-8 h-8 rounded-full bg-zinc-500">
+          <span className="text-zinc-100 w-full h-full flex items-center justify-center">
+            {firstLetter}
+          </span>
+        </div>
+        <span>{user?.name}</span>
+      </div>
+
       <Button onClick={() => [signout(), navigate("/")]}>Sair</Button>
     </div>
   );
